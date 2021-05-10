a0c3ea920e895630a7e5a613a4e0f7886464ece0
Nirman Narang <nirmannarang@gmail.com>
2021-05-10 20:48:27 +0530

    Update ping_script.sh

--- ping_script.sh
+++ ping_script.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 flagger=0
 count=0
+chmod +x cowin_notifier.py
 while true
 do
-	if pidof -x "vaccine_spotter.py" >/dev/null; then
+	if pidof -x "cowin_notifier.py" >/dev/null; then
     		echo "Process already running"
 		if [ $flagger -eq 0 ]
 		then
-			telegram-send "Resolved: AWS Vaccine Spotter is up now."
+			telegram-send "Resolved: AWS Slot Notifier is up now."
 			flagger=1
 			count=0
 		fi
     	else
-    		echo "!!!!Urgent!!!!  AWS Vaccine Spotter is down."
-    		telegram-send "!i!i!i! Urgent !i!i!i!  AWS Vaccine Spotter is down."
+    		echo "!!!!Urgent!!!!  AWS Slot Notifier is down."
+    		telegram-send "!i!i!i! Urgent !i!i!i!  AWS Slot Notifier is down."
 		flagger=0
 		count=$((count + 1))
 	fi
 
 	if [ $count -gt 2 ]
 	then
-		nohup ./vaccine_spotter.py &
+		nohup ./cowin_notifier.py &
 	fi
 	sleep 20
-	sudo free -mh && sudo sync && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches' && sudo free -mh
+	sudo sync && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
 done
